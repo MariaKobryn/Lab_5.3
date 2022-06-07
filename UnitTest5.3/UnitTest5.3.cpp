6b8e4f92115c42cae0804dd453587710d36090fb
MariaKobryn <kobinmary2508@gmail.com>
2022-06-07 03:02:37 +0300

    first master commit

---
+++ UnitTest5.3/UnitTest5.3.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_5.3/Lab_5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest53
+{
+	TEST_CLASS(UnitTest53)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			A a;
+			a.setX(18);
+			Assert::AreEqual(18, a.getX());
+		}
+	};
+}
